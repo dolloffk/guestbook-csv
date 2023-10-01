94afbcd1b1d0a81b754f7495f5a4a6e4f0f04267
kayle <kaylee@dolloff.net>
2023-10-01 15:23:35 -0600

    Fix pagination

--- index.php
+++ index.php
@@ -7,12 +7,14 @@ $entries = dateSort(toArray($filepath));
 $total_entries = countComments($entries);
 $page = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1;
 $total_pages = ceil($total_entries/$per_page);
+$calc_page = ($page - 1) * $per_page;
+$page_entries = array_slice($entries, $calc_page, $per_page);
 
 if ($total_entries == 0) {
     echo $noentries;
 } else {
     showPages($page, $total_pages);
-    foreach ($entries as $rows) {
+    foreach ($page_entries as $rows) {
         $name = $rows['name'];
         $url = $rows['url'];
         $date = date_create($rows['date']);
