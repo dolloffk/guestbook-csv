9a439b0bd9640747e0f874bf0d51cdd2e27b8bd7
kayle <kaylee@dolloff.net>
2023-11-12 21:19:58 -0700

    Fix HTML error in sign form

--- sign.php
+++ sign.php
@@ -5,9 +5,9 @@
 <h2>Sign guestbook</h2>
 <?php echo $msg; ?>
 <form id="comment" method="post" action="sign.php">
-<div><label for="username">Name</label><br/><input type="text" name="username" role="username" placeholder="Name" value="<?php echo $_SESSION['name']; ?>"></input></div>
-<div><label for="website">Website (optional)</label><br/><input type="text" name="website" role="website" placeholder="http://" value="<?php echo $_SESSION['url']; ?>"></input></div>
-<div><label for="security"><?php echo $securityq; ?></label><br/><input type="text" name="security" role="security" placeholder="yes/no"></input></div>
+<div><label for="username">Name</label><br/><input type="text" name="username" role="username" placeholder="Name" value="<?php echo $_SESSION['name']; ?>" /></div>
+<div><label for="website">Website (optional)</label><br/><input type="text" name="website" role="website" placeholder="http://" value="<?php echo $_SESSION['url']; ?>" /></div>
+<div><label for="security"><?php echo $securityq; ?></label><br/><input type="text" name="security" role="security" placeholder="yes/no" /></div>
 <div><label for="comment">Comment</label><br/>
 <textarea rows="6" name="comment"><?php echo $_SESSION['comment']; ?></textarea></div>
 
